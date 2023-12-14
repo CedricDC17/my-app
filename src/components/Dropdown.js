bbb684f68245f3a7cf7fec5b238d22bda00470c0
CedricDC17 <cedric.dc91@gmail.com>
2023-12-14 03:42:19 +0100

    arrow Dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import styles from '../components/Dropdown.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const Dropdown = ({ buttonText, contentText , style}) => {
+
+
+const Dropdown = ({ buttonText, contentText, style }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
 
+    const arrowClass = isOpen ? `${styles.arrow} ${styles.activeArrow}` : styles.arrow;
+    const arrow = <FontAwesomeIcon className={arrowClass} icon={faChevronDown} />
     const contentClass = isOpen ? `${styles.dropdownContent} ${styles.active}` : styles.dropdownContent;
 
     return (
         <div className={styles.dropdown}>
-            <button className={styles.dropbtn} onClick={toggleDropdown}>
-                {buttonText}
-            </button>
+            <div className={styles.DropdownbuttonContainer}>
+                <button className={styles.dropbtn} onClick={toggleDropdown}>
+                    {buttonText}
+                    {arrow}
+                </button>
+            </div>
             <div className={`${contentClass} ${style}`}>
                 <div className={styles.dropContainer}>
                     {Array.isArray(contentText) ? (
