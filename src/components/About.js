704547260f5f96515114c9641ee3aebd0aea8c5b
CedricDC17 <cedric.dc91@gmail.com>
2024-01-08 14:21:51 +0100

    Fix typo in About.js dropdown content

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
       <div className={styles.dropdownContainer}>
         <Dropdown buttonText="FIabilité" contentText="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
         <Dropdown buttonText="Respect" contentText="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme. " />
-        <Dropdown buttonText="Service" contentText="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+        <Dropdown buttonText="Service" contentText="La bienveillance fait partie des valeurs fondatrices de Kaza. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
         <Dropdown buttonText="Sécurité" contentText="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
       </div>
     </div>
